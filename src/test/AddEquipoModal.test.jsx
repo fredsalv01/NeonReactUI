2389e6941da56e077d01f8e23b892d2e194ef63a
Freddy Morales <freddy.morales@workcapit.us>
2026-06-03 18:09:36 -0500

    test: add AddEquipoModal test component to diagnose input focus loss issue

--- src/test/AddEquipoModal.test.jsx
+++ src/test/AddEquipoModal.test.jsx
@@ -4,7 +4,7 @@
  */
 
 import React, { useState } from 'react'
-import { AddEquipoModal } from './src/components/dashboard/modals/AddEquipoModal'
+import { AddEquipoModal } from '../components/dashboard/modals/AddEquipoModal'
 
 // Standalone test component
 export const AddEquipoModalTest = () => {
